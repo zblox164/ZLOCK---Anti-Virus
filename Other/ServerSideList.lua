ba611b7a5358ea8ad1e02f9f7e67aa322bee6f88
zblox164 <68709543+zblox164@users.noreply.github.com>
2021-11-30 16:53:32 -0600

    Create ServerSideList.lua

---
+++ Other/ServerSideList.lua
@@ -0,0 +1,13 @@
+local list = {}
+
+list.serverSides = {
+	"BoringSS", "RainbowSS", "BuilderX", "serverside", "Dex", "neb", "Branch",
+	"DarknessSSV1", "Trollemius", "Vanity", "Alpha SS", "Vexaton",
+	"Synapse", "ScriptHub (Dont Change)", "ENVY", "Aureus",
+	"DarkHub", "LoadSinner", "Exoliner", "milkss", "SnakeVenomSS", "SynX", 
+	"SS-Executor", "PhoenixSS", "JOHNDOE", "ExecutorMain", "SSynapseX", "SadsSSGui",
+	"1x1x6x5x3's SS exe (put in startergui)", "pogclient", "Server Sided Executor",
+	"Sinner"
+}
+
+return list
