c93bc9235fde7c38c41050b22d6752c8ea8b14df
zblox164 <68709543+zblox164@users.noreply.github.com>
2021-11-30 16:54:08 -0600

    Create VirusList.lua

---
+++ Other/VirusList.lua
@@ -0,0 +1,73 @@
+local list = {}
+
+list.virusNames = {
+	"4D Being", "4DBeing",
+	"_ZVN",
+	"sݣ����-.G�t",
+	
+	"AntiLag", "Anti Cheat Engine!", "Anti Lag", "Anti-Lag", "Anti-Lag",  "ANTILAG script", "Anti-Lag2",  
+	"Anti-Lag Pack {READ}", "AntiLag2", "Anti Lag2", "AntiVirusSoftware",
+	"Anti-Lag v 7.5", "ANTIVIRISIS", "ANTIVIRIS", "Antivirus", "ARBITER47",
+	
+	"Bob", "B0B",
+	
+	"C-REX",
+	
+	"dââââââââng you got owned", "Deth 2 teh samurai!", "Dont Worry Im A Friendly Virus",
+	"d��������������ng.........you got owned...", "d??????????????ng.........you got owned...",
+	"dï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ng.........you got owned...", "Dupe",
+	
+	"ExecutorServer", "EXTRAHAX",
+	
+	"FeelFreeToIns3rtGramm�tic�lErrorsHere", "FreeStyleM�yGoAnywhereIfNeeded",
+	"Fire", "Fire Remover",
+	
+	"garmo hacked ur place", "Glosware� | Anti-Fire", "Guest Free Chat Script", "Guest Talking Script", 
+	"Guest_Talking_Script",
+	
+	"H4XXX", "HAAXEDBYCREX", "Hack", "Harmless little scripty", "Hidden",
+	
+	"I am a friendly virus", "IAmHereToHe�lYourPlace", "IAmHereToHe?lYourPlace",
+	"Important,Breaks if you open.", "IsAVirus",
+	"infection", "Infected", "Installer", "Inject", "injector", "IMDOINGITOKSODONTKILLMENOW", "IsInfected",
+	"InfectedScript",
+	
+	"join teh moovment!",
+	
+	"Kraftyisback", "Kill tem!",
+	
+	"Lag", "Lag Script", "Lolzorz", "luraph",
+	
+	"micolord",
+	
+	"No samurai plzzz", "N0ISE", "Nice little scripty",
+	"NoNoIDon'tNeedAllOfYourAwkw�rdSovietArguments",
+	
+	"oDPAKSFOPASKDOPKospkd",
+	"OMG ITS A VIRUS", "OH SNAP YOU GOT INFECTED XD XD XD", "OHAI",
+	
+	"protection", "ProperGr�mmerNeededInPhilosiphalLocations;insertNoobHere",
+	"ProperGrï¿½mmerNeededInPhilosiphalLocations;insertNoobHere",
+	
+	"RemoteHandler", "ResponseSystem", "ROFL", "RoSync", "RSFfacility",
+	
+	"ServerSide", "Server Side", "Script......Or is it...", 
+	"SnapReducer", "Snap Reducer", "Snap-Reducer", "snap infection",
+	"snapinfection", "Snap-infection",
+	"Spread", "Spreadify", "Sovietify", "soz i herd u lik mudkipz",
+	
+	"TehScript", "Trashed", "ThisScriptIsAJumpStartToAHe�lthyLifestyle",
+	
+	"Vaccine", "Vacicne", "Virus", "Virus_666.exe", "ViVRuS", "ViVrus", "Vivurursdd", "Virus Protect",
+	"VirusScript", "Virus Script", "VK� 2011",
+	
+	"WelderPart", "Wildfire", "Wormed", "Worm",
+	
+	"XXCHAINSXX2477 WUS HERE!!!!!                                                      Krodmiss V15153   ",
+	
+	"Yew've Been UMG'd!", "YOUR COMPUTER HAS A VIRUS", "your screwed, u cant scan this virus",
+	
+	"zackary1997", "Zackisk"
+}
+
+return list
