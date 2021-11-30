e9b97d563e249b01a478b639b78b322f382240d9
zblox164 <68709543+zblox164@users.noreply.github.com>
2021-11-30 16:53:15 -0600

    Create JunkList.lua

---
+++ Other/JunkList.lua
@@ -0,0 +1,13 @@
+local list = {}
+
+list.deprecated = {
+	[":connect"] = "Deprecated function connection", [":Remove"] = ":Remove()", 
+	[":remove"] = ":remove()", ["Draggable"] = "Deprecated property 'Draggable'", 
+	[":clone"] = ":Deprecated clone", [":findFirstChild"] = "Deprecated FindFirstChild()",
+	[":destroy"] = "Deprecated Destroy()", [":isA"] = "Deprecated IsA()",
+	[":getChildren"] = "Deprecated GetChildren()", [":children"] = "Deprecated function 'children()'",
+	[":isDescendantOf"] = "Deprecated IsDescendantOf()", [".childAdded"] = "Deprecated signal 'childAdded'",
+	["className"] = "Deprecated property 'className'"
+}
+
+return list
