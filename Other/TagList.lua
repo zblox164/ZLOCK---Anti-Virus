93c8b5393ba8e7966b523336dbac667f137dcb40
zblox164 <68709543+zblox164@users.noreply.github.com>
2021-11-30 16:53:50 -0600

    Create TagList.lua

---
+++ Other/TagList.lua
@@ -0,0 +1,76 @@
+local list = {}
+
+list.tags = {
+	["RemoteHandler"] = "Server side exploit", 
+	["AlphaSS"] = "Server side exploit", ["Vexaton"] = "Server side exploit", 
+	["Synapse"] = "Server side exploit", ["AccessUI"] = "Server side exploit",
+	["parkur"] = "Server side exploit", ["BuilderX"] = "Server side exploit", 
+	["ReptionSS"] = "Server side exploit", ["DarkHub"] = "Server side exploit",
+	["milkssexecutor"] = "Server side exploit", ["SynX"] ="Server side exploit",
+	["ExecutorMain"] = "Server side exploit", ["SadsSSGui"] = "Server side exploit",
+	["pogclient"] = "Server side exploit", ["SS-Executor"] = "Server side exploit", 
+	["1x1x6x5x3's SS exe (put in startergui)"] = "Server side exploit",
+	["LeakedServerSideSource"] = "Server side exploit", 
+	["RainbowSS"] = "Server side exploit",
+	
+	[".load("] = ".load()", [":Run("] = ":Run()", 
+	[".Execute("] = ".Execute()", [":mbye("] = ":mbye()",
+	 
+	["2849585171"] = "Virus require", ["3023538289"] = "Virus require", 
+	["2965793312"] = "Virus require", ["3041175937"] = "Virus require", 
+	["7080452551"] = "Virus require", ["7623919572"] = "Virus require",
+	["7058538406"] = "Virus require", ["4450207463"] = "Virus require", 
+	["6732534518"] = "Virus require", ["7054635595"] = "Virus require",
+	["3688625160"] = "Virus require", ["6865434978"] = "Virus require",
+	["4832971989"] = "Virus require", ["3747589551"] = "Virus require",
+	["5356833426"] = "Virus require", ["1232007386"] = "Virus require",
+	["5517771478"] = "Virus require", ["5032156830"] = "Virus require",
+	["5767502606"] = "Virus require", ["4781464455"] = "Virus require",
+	["5225767191"] = "Virus require", ["4690715583"] = "Virus require",
+	["6352196759"] = "Virus require", ["5119713224"] = "Virus require",
+	["2595582545"] = "Virus require", ["533497416"] = "Virus require",
+	["2809322599"] = "Virus require", ["5516390670"] = "Virus require",
+	["4877907950"] = "Virus require",
+	
+	["Mil3005"] = "Known Virus", ["ss1122"] = "Known Virus", 
+	["N0ISESCRIPT"] = "Known Virus", ["OpArgR"] = "Known Virus", 
+	["RobuxGenerator"] = "Known Virus", ["Vaccine"] = "Known Virus",
+	["RSFfacility"] = "Known Virus", ["Virus_666.exe"] = "Known Virus",
+	["infection"] = "Known Virus", ["C-REX"] = "Known Virus", ["crex"] = "Known Virus",
+	["orbuzzingnoise"] = "Known Virus", ["CRUSHER4003"] = "Known Virus", 
+	["ResponseSystem"] = "Known Virus", ["Infected"] = "Known Virus", ["RoSync"] = "Known Virus",
+	["iEmotionless's Lag Script"] = "Known Virus", ["Infector"] = "Known Virus", 
+	["luraph"] = "Known Virus",
+	
+	
+	["script:Clone("] = "script:Clone()",
+	['Instance.new("Hint"'] = "Inserts 'Hint' objects",
+	["TeleportService"] = "Accesses TeleportService",
+	
+	["3tx.live"] = "Unwanted website link", 
+	 
+	["discord.gg"] = "Discord link",
+	
+	["IllIIIlIIIIll"] = "Unreadable script", 
+	["lIlIllIlIlll"] = "Unreadable script", 
+	["llllIIIlllllll"] = "Unreadable script",
+	["IlI"] = "Unreadable script",
+	["IIl"]  = "Unreadable script",
+	["III"] = "Unreadable script",
+	["lII"] = "Unreadable script",
+	
+	
+	["require("] = "require()", 
+	["loadstring("] = "loadstring()", 
+	["getfenv("] = "getfenv()", 
+	["PostAsync("] = "PostAsync()", 
+	["IsStudio("] = "IsStudio()",
+	
+	["WEBHOOK_LINK"] = "Unwanted website webhook",
+	["webhook"] = "Unwanted website webhook",
+	
+	["virus"] = "Keyword 'virus'",
+	["serverside"] = "Keyword 'serverside'",
+}
+
+return list
