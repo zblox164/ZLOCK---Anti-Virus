111de1dcb2e7da8d30d73f55a6a5ec9e418fd8d3
zblox164 <68709543+zblox164@users.noreply.github.com>
2022-01-23 15:20:37 -0600

    Update ServerSideList.lua

--- Other/ServerSideList.lua
+++ Other/ServerSideList.lua
@@ -5,9 +5,9 @@ list.serverSides = {
 	"DarknessSSV1", "Trollemius", "Vanity", "Alpha SS", "Vexaton",
 	"Synapse", "ScriptHub (Dont Change)", "ENVY", "Aureus",
 	"DarkHub", "LoadSinner", "Exoliner", "milkss", "SnakeVenomSS", "SynX", 
-	"SS-Executor", "PhoenixSS", "JOHNDOE", "ExecutorMain", "SSynapseX", "SadsSSGui",
+	"SS-Executor", "PhoenixSS", "ExecutorMain", "SSynapseX", "SadsSSGui",
 	"1x1x6x5x3's SS exe (put in startergui)", "pogclient", "Server Sided Executor",
-	"Sinner"
+	"Sinner", "server side"
 }
 
 return list
